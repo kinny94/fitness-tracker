14ae3bc612a939b2595969d414348135d6acb400
kinny94 <ad.sam007@gmail.com>
2018-09-05 16:25:53 -0400

    implemented the login form with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { NgModule } from '@angular/core';
 
@@ -33,7 +33,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
 		MaterialModule,
 		AppRoutingModule,
 		FlexLayoutModule,
-		FormsModule
+		FormsModule,
+		ReactiveFormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	loginForm: FormGroup;
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.loginForm = new FormGroup({
+			email: new FormControl('', [
+				Validators.required,
+				Validators.email
+			]),
+			password: new FormControl('',[
+				Validators.required
+			])
+		})
+	}
+
+	onSubmit(){
+		console.log( this.loginForm );
+	}
 
 }

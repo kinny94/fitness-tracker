6d2a349de524c6e0e45eadbcd1d4795c5f3655d4
kinny94 <ad.sam007@gmail.com>
2018-09-05 12:07:27 -0400

    Added angular material and routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule, Router } from "@angular/router";
+
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+	{
+		path: '',
+		component:  WelcomeComponent
+	},
+	{
+		path: 'signup',
+		component: SignupComponent
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: 'training',
+		component: TrainingComponent
+	}
+];
+
+@NgModule({
+	imports: [ RouterModule.forRoot( routes ) ],
+	exports: [ RouterModule ]
+})
+
+export class AppRoutingModule{
+
+}

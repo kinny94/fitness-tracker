038b70c24d52c1a92cc1bd54215aa6befb78859b
kinny94 <ad.sam007@gmail.com>
2018-09-06 09:55:09 -0400

    added sidenav and toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fitness-tracker';
+	title = 'fitness-tracker';
+
+	onToggle(){
+
+	}
 }
